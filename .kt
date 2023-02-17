dfc1915fd086b8462e60e7caa6cb4621d9a31da3
Darian-Cătălin Cucer <86965240+cucerdariancatalin@users.noreply.github.com>
2023-02-17 13:27:30 +0200

    Create .kt
    Explanation:
    
    The val keyword is used to declare a read-only variable (similar to final in Java).
    In the first example, a val variable name is declared with the value "John". This variable cannot be reassigned to a different value.
    In the second example, a val variable numbers is declared with a list of numbers. This variable is also read-only and cannot be reassigned, but the elements of the list can be modified.
    In the third example, val variables x and y are declared with values 10 and 20, respectively. A third val variable z is declared with the sum of x and y. The val keyword is not required for z, but it is good practice to use it for all read-only variables.

---
+++ .kt
@@ -0,0 +1,13 @@
+// Examples of val in Kotlin
+fun main() {
+    val name = "John"
+    println("Hello, $name!")
+
+    val numbers = listOf(1, 2, 3, 4, 5)
+    println("The numbers are $numbers")
+
+    val x: Int = 10
+    val y: Int = 20
+    val z = x + y
+    println("The sum of $x and $y is $z")
+}
